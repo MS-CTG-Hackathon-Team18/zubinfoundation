6c47f2180031a05f9f3a40e219d2f7f6b2023bcb
XUjiayi777 <jxugl777@gmail.com>
2024-08-24 14:06:28 +0800

    merge

--- src/app/main/events/page.js
+++ src/app/main/events/page.js
@@ -7,28 +7,44 @@ const EventsPage = () => {
   return (
     <>
       <Navigationbar />
-      <div className="font-sans font-bold flex flex-col justify-between mt-20 rounded-lg px-44">
-        <div className="mb-6 flex flex-col">
-          <span className="text-black text-3xl font-sans font-bold">
+      <div className="font-sans font-bold flex flex-col justify-between my-10 rounded-lg mx-20">
+        <div className="mb-5">
+          <h2 className="scroll-m-20 pb-2 text-3xl font-bold tracking-tight">
             Events
-          </span>
-          <span className="text-neutral-500 text-sm font-sans ">
+          </h2>
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-gray-400">
             Browse what's happening near you
-          </span>
+          </h4>
         </div>
 
-        <div className="flex flex-wrap align-middle">
-          <EventCard Picture={ImageTesting1} />
-          <EventCard Picture={ImageTesting2} />
-          <EventCard Picture={ImageTesting1} />
-          <EventCard Picture={ImageTesting1} />
-          <EventCard Picture={ImageTesting1} />
-          <EventCard Picture={ImageTesting1} />
-          <EventCard Picture={ImageTesting1} />
-          <EventCard Picture={ImageTesting1} />
-          <EventCard Picture={ImageTesting1} />
-          <EventCard Picture={ImageTesting1} />
-          <EventCard Picture={ImageTesting1} />
+        <div className="flex flex-wrap align-center justify-center">
+          <div className="my-3 mx-5">
+            <EventCard Picture={ImageTesting1} />
+          </div>
+          <div className="my-3 mx-5">
+            <EventCard Picture={ImageTesting2} />
+          </div>
+          <div className="my-3 mx-5">
+            <EventCard Picture={ImageTesting1} />
+          </div>
+          <div className="my-3 mx-5">
+            <EventCard Picture={ImageTesting1} />
+          </div>
+          <div className="my-3 mx-5">
+            <EventCard Picture={ImageTesting1} />
+          </div>
+          <div className="my-3 mx-5">
+            <EventCard Picture={ImageTesting1} />
+          </div>
+          <div className="my-3 mx-5">
+            <EventCard Picture={ImageTesting1} />
+          </div>
+          <div className="my-3 mx-5">
+            <EventCard Picture={ImageTesting1} />
+          </div>
+          <div className="my-3 mx-5">
+            <EventCard Picture={ImageTesting1} />
+          </div>
         </div>
       </div>
     </>

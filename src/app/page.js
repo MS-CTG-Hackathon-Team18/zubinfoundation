dcebfb5e8d29f5043be8026e2f7d58b2c6567f1f
tompark58981 <kyunuya@github.com>
2024-08-26 01:56:11 +0900

    set up sessions

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
 import Navigationbar from "@/components/Navigationbar";
 import IntroductionPage from "./introduction/page";
 
+export default function Page() {
   return (
     <>
       <Navigationbar />
       <IntroductionPage />
-
-      
     </>
   );
 }
